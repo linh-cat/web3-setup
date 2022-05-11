b9f6e88952a1e729b69af92702b2d5c069a1ed48
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2022-05-11 21:46:02 +0700

    setup code dapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Web3 from "web3";
 
 function App() {
+  const [account, setAccount] = useState();
+  const [network, setNetwork] = useState();
+  const [balance, setBalance] = useState();
+  const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+
+  async function loadAccount() {
+    const accounts = await web3.eth.requestAccounts();
+    setAccount(accounts[0]);
+  }
+  async function loadBalance() {
+    const network = await web3.eth.net.getNetworkType();
+    const balance = await web3.eth.getBalance(account, "latest");
+    setBalance(balance);
+    setNetwork(network);
+  }
+
+  useEffect(() => {
+    loadAccount();
+  }, []);
+
+  useEffect(() => {
+    console.log("switch");
+    loadBalance();
+  }, [account]);
+
+  console.log(account);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>my account {account}</p>
+      <p>network {network}</p>
+      <p>balance {balance}</p>
     </div>
   );
 }
